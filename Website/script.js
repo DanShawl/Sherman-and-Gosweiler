dd2de82804cda250d4485fde8946b8f50f1002f3
Dan Shawl <danielrshawl@gmail.com>
2021-04-26 22:13:26 -0400

    Initial Commit for Sherman and Gosweiler

---
+++ Website/script.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper('.swiper-container', {
+  // slidesPerView: 1.5,
+  slidesPerView: 2,
+  // slidesPerView: 1.35,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+var swiper2 = new Swiper('.swiper-container-new', {
+  // slidesPerView: 1.5,
+  slidesPerView: 1.5,
+  // slidesPerView: 1.35,
+  spaceBetween: 10,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
